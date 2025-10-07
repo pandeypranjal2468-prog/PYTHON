2c4dfe7a6504b22db8e5d8d0c16d1091518f14e4
PRANJAL PANDEY <pandeypranjal2468@gmail.com>
2025-10-07 21:01:05 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first no."))
+b=int(input("enter the second no"))
+add=a+b
+subtract=a-b
+multiply=a*b
+divide=a/b 
+print("addition",add)
+print('subtraction',subtract)
+print('multiplication',multiply)
+print('divide',divide)
